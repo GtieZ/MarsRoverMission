5bfb0c41350daf7a5f4c18d5793a10704b11617e
Angel Gtiez <angel.gutz.gtiez@gmail.com>
2022-02-05 11:14:52 +0100

    adding cell class

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 
-function setup(){
+class Cell {
+  constructor (x, y, size){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
 
-  createCanvas(500, 500);
+   draw(){
+     strokeWeight(1);
+    stroke(255);
+    noFill(255);
+    rect(this.x,this.y, this.size, this.size);
+  }
+
+}
+
+
+
+
+ let myCell = new Cell(10,10, 50);
+ console.log(myCell)
+
+
+
+function setup(){
+  createCanvas(600, 600);
   background(0);
+}
+
+function draw(){
+
+  myCell.draw();
+
 
 }
